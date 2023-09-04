e736bc6a1a3c21d30138ecf025ba325dba3710b8
StphnX <136083929+StphnX@users.noreply.github.com>
2023-09-04 15:44:43 +0200

    final changes to get all Pokemon data

--- Server-PokeFight/Controllers/pokemonController.js
+++ Server-PokeFight/Controllers/pokemonController.js
@@ -1,7 +1,24 @@
 import asyncHandler from "express-async-handler";
+import Pokemon from "../schemas/pokemonSchema.js";
+
 
 const getAllPokemon = asyncHandler(async (req, res, next) => {
-  res.status(200).send("Get all Pokemon route working");
+  const { query } = req.query;
+
+  if (query) {
+    const results = await Pokemon.aggregate().search({
+      index: "pokemon",
+      text: {
+        query,
+        path: ["name", "type"],
+      },
+    });
+    res.status(200).json(results);
+  } else {
+    const results = await Pokemon.find({}, { sprites: 1, stats: 1, pokedexId: 1, name: 1, _id: 0 });
+
+    res.status(200).json(results);
+  }
 });
 
 const getSinglePokemon = asyncHandler(async (req, res, next) => {
