cfe9400935357c9de8228ce598c43317f83a6d6b
StphnX <136083929+StphnX@users.noreply.github.com>
2023-09-04 16:02:36 +0200

    added GET single Pokemon and Pokemon by id/prop

--- Server-PokeFight/Controllers/pokemonController.js
+++ Server-PokeFight/Controllers/pokemonController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import Pokemon from "../schemas/pokemonSchema.js";
 
-
+//GET all pokemon
 const getAllPokemon = asyncHandler(async (req, res, next) => {
   const { query } = req.query;
 
@@ -21,27 +21,54 @@ const getAllPokemon = asyncHandler(async (req, res, next) => {
   }
 });
 
+
+// GET single pokemon
+
 const getSinglePokemon = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
 
-  const isIdNotName = Number(id);
+  const searchProp = Number(id) ? "pokedexId" : "name";
+  const searchValue = Number(id) || /id/i;
+  const filter = { [searchProp]: searchValue };
 
-  res
-    .status(200)
-    .send(`Requested pokemon: ${isIdNotName ? "ID" : "Name"} ${id}`);
+  const result = (await Pokemon.findOne(filter)) ?? {};
+
+  if (!result) {
+    return res.status(400).json({
+      message: `Query for ${searchProp}: ${searchValue} did not return any results.`,
+    });
+  }
+
+  res.status(200).json(result);
 });
 
 const getSinglePokemonInfo = asyncHandler(async (req, res, next) => {
   const { id, prop } = req.params;
 
-  const isIdNotName = Number(id);
+  const searchProp = Number(id) ? "pokedexId" : "name";
+  const searchValue = Number(id) || /id/i;
 
-  res
-    .status(200)
-    .send(
-      `Requested pokemon: ${isIdNotName ? "ID" : "Name"
-      } ${id}, requesting prop ${prop}`
-    );
+  const filter = { [searchProp]: searchValue };
+  const projection = { [prop]: 1 };
+
+  const result = await Pokemon.findOne(filter, projection);
+
+  if (!result) {
+    return res.status(400).json({
+      message: `Query for ${searchProp}: ${searchValue} did not return any results.`,
+    });
+  }
+
+  let retObj = result.toObject();
+
+  // if the prop we are looking for does not exist, this endpoint
+  // should not return anything... but mongoDB always returns an Object
+  // with a single property if the document exists (property _id)
+  if (Object.keys(retObj).length === 1) {
+    retObj = {};
+  }
+
+  res.status(200).json(retObj);
 });
 
 export { getAllPokemon, getSinglePokemon, getSinglePokemonInfo };
