5459b428009b26975486db658330f2da9df04ad3
susuvii <susanna.viitanen91@gmail.com>
2023-09-15 13:42:09 +0200

    button changes

--- Client-PokeFight/src/components/PokeCard.jsx
+++ Client-PokeFight/src/components/PokeCard.jsx
@@ -6,21 +6,21 @@ const PokeCard = ({allPokemons}) => {
     const {winner, pokedexId} = useParams()
     const navigate = useNavigate();
     
-    // const handleClick = () => {
-    //     console.log("get me out of here")
-    //     navigate("/api/pokemon");
-    // } 
+    const handleClick = () => {
+        console.log("get me out of here")
+        navigate("/api/pokemon");
+    } 
 
     
     
 // TODO button to start over 
 
   return (
-    <div >
+    <div className="fight-again-container">
         <h2>THE WINNER IS {winner}!!!</h2>
                 {/* <img src={pokemon.sprites.artwork}/> */}
-        <button onClick={() =>{console.log('get me outta here!!!')}}>Start new fight</button>
-        <button onHover={() => {console.log('wtf is going on')}}>new button</button>
+        <button onClick={handleClick()}>Start new fight</button>
+        <button onClick={handleClick()}>new button</button>
     </div>
 
   )
